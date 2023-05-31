eb1d8c055767ab9df442b020b0be7269bdf20b02
Aravind142857 <aravind.s2025@berkeley.edu>
2023-05-30 22:03:50 -0500

    Update With some tile images

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ function Header() {
     return (
     <nav id="navbar">
         <ul>
-            <li><img src="tile000.png"></img></li>
-            <li>Courses</li>
-            <li>Projects</li>
-            <li>Links</li>
+            <li><a href={"./index.html"}><img src="tile000.png"></img></a></li>
+            <li><a href={"./courses.html"}>Courses</a> </li>
+            <li><a href={"./projects.html"}>Projects</a></li>
+            <li><a href={"./skills.html"}>Skills</a></li>
+            <li><a href={"#foot"}>Links</a></li>
         </ul>
     </nav>
     )
 }
 
+function Footer() {
+    return (
+        <ul>
+            <li>Contact me @</li>
+            <li><a href={"https://www.instagram.com/aravind_02_10"}><img className="image" src="instagram_light.png" alt="instagram Dark"></img></a></li>
+            <li><a href={"https://github.com/Aravind142857"}><img className="image" src="github_light.png" alt="github.png"></img></a></li>
+            <li><a href={"https://www.linkedin.com/in/aravindsrinivasan02"}><img className="image" src="linkedin_light.svg" alt="linkedIn.png"></img></a></li>
+            <li id={"go-up"}><a href={"#head"}>TOP &#8593;</a></li>
+        </ul>
+    )
+}
 ReactDOM.render(<Header />, document.getElementById("head"))
+ReactDOM.render(<Footer />, document.getElementById("foot"))
